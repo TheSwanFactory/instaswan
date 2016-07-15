8ad6a479c462687930f5ad1dea894b50da5ba06d
Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-07-14 20:45:36 -0700

    List of effects

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,18 @@
+
 class Photo < ApplicationRecord
+    EFFECTS = %w(
+        charcoal
+        emboss
+        implode
+        oil_point
+        polaroid
+        sepiatone
+        shadow
+        sketch
+        solarize
+        swirl
+        vignette
+        wave
+        wet_floor
+    )
 end
