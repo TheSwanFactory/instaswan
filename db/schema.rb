705f11884421d7342d5d313d3c90cf09b35e6846
Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-07-21 20:51:31 -0700

    generate devise user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708052433) do
+ActiveRecord::Schema.define(version: 20160722035014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -794,6 +794,23 @@ ActiveRecord::Schema.define(version: 20160708052433) do
     t.integer "owner_id"
     t.index ["company_id"], name: "user_profiles_ownership_447d3092", using: :btree
     t.index ["owner_id"], name: "user_profiles_ownership_5e7b1936", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "account_emailaddress", "accounts_typeuser", column: "user_id", name: "account_emailaddress_user_id_2c513194_fk_accounts_typeuser_id"
