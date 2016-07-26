596ef19fef0ccd6a63adb86bbc18645f20008327
Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-07-25 17:36:02 -0700

    Save user with photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -30,6 +30,7 @@ class PhotosController < ApplicationController
   # POST /photos
   # POST /photos.json
   def create
+    photo_params[:user] = current_user
     @photo = Photo.new(photo_params)
 
     respond_to do |format|
